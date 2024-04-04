27aed96e6939b7cb2280c68a3b460f2446dce129
Jarvanator07 <140124249+Jarvanator07@users.noreply.github.com>
2024-04-04 23:39:19 +0000

    i might just

--- connect-four.py
+++ connect-four.py
@@ -1,5 +1,15 @@
-def main():
-    # Your code goes here
+board = []
 
-if __name__ == "__main__":
-    main()
+for i in range(6):
+    board.append(["_","_","_","_","_","_"])
+
+for row in board:
+    for colum in row:
+        print(colum, end=' ')
+    print()
+
+int(input("Enter your choice(column 1-6): "))
+if int(input()) in range(1,7):
+    board[0][int(input())-1] = "X"
+else:
+    print("Invalid input")
