e28db34dd7ed9d4dec3e448065f9398be99bc6ac
Jarvanator07 <140124249+Jarvanator07@users.noreply.github.com>
2024-04-10 00:38:45 +0000

    buss

--- connect-four.py
+++ connect-four.py
@@ -1,34 +1,40 @@
 board = []
 
 for i in range(6):
-    board.append(["_","_","_","_","_","_"])
+    board.append(["_","_","_","_","_","_","_"])
 
-for row in board:
-    for colum in row:
-        print(colum, end=' ')
-    print()
+while True:
 
-int(input("Enter your choice(column 1-6): "))
-if int(input()) in range(1,7):
-    board[0][int(input())-1] = "X"
-else:
-    print("Invalid input")
+    starting_player = input("Do you want to play as X or O? ")
+    if starting_player == "X" or starting_player == "O":
+        break
+    else:
+        print("Invalid input")
+    
+    
 
-def placeToken(board, column, player):
+    # to print the board
+    for row in board:
+        for colum in row:
+            print(colum, end=' ')
+        print()
 
-    for i in range(5, -1, -1):
+    tokenX = 'X'
+    tokenO = 'O'
+    column = int(input("Enter your choice(column 1-6): "))
 
-        if board[i][column] == '-':
-
-            # Place the token
-            board[i][column] = player
+    # to place the token
+    for row in range(5, -1, -1):
+        if board[row][column] == "_":
+            board[row][column] = tokenO 
             break
 
-for row in range(4):
-    for colum in range(6):
-        if board[row][colum] == board[row][colum + 1] == board[row][colum + 2] == board[row][colum + 3] and not board[row][colum + 3] == "_":
-            print("Player X wins")
-            break
-        elif board[row][colum] == board[row+1][colum] == board[row+2][colum] == board[row+3][colum] == "O":
-            print("Player O wins")
-            break
+    # to check for win
+    for row in range(6):
+        for colum in range(4):
+            if board[row][colum] == board[row][colum + 1] == board[row][colum + 2] == board[row][colum + 3] == "X":
+                print("Player X wins")
+                break
+            elif board[row][colum] == board[row][colum + 1] == board[row][colum + 2] == board[row][colum + 3] == "O":
+                print("Player O wins")
+                break
